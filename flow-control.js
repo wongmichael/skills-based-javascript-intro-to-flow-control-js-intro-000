e29409edf3ba3c9e0be6ba201a3b6adf78d94051
Michael Wong <mik9080@gmail.com>
2017-10-11 15:46:25 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,8 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case age>=13 && age<=19:
+    case age>=13:
+    case age<=19:
       return "You are a teenager"
     default:
       return "You have an age"
